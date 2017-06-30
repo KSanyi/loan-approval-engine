e4261a6ea715d73d5f564fd536b70a07b8646e29
Sandor Gabor Kocso <kocso.sandor.gabor@gmail.com>
2017-06-30 16:23:46 +0200

    update demo fin hist data

--- src/main/java/hu/lae/domain/Client.java
+++ src/main/java/hu/lae/domain/Client.java
@@ -49,16 +49,13 @@ public class Client {
     public static Client createDefault() {
         
         FinancialStatementData financialStatementData2016 = new FinancialStatementData(2016, 
-                new BalanceSheet(new Assets(400, 50, 20, 30), new Liabilities(1000, 100, 70, 0, 2000)), 
-                new IncomeStatement(600, 300, 70, 30, 300));
+                new BalanceSheet(new Assets(400, 50, 20, 30), new Liabilities(1000, 100, 70, 0, 2000)), new IncomeStatement(2000, 300, 70, 30, 300));
         
         FinancialStatementData financialStatementData2015 = new FinancialStatementData(2015, 
-                new BalanceSheet(new Assets(300, 40, 10, 20), new Liabilities(900, 80, 60, 0, 1500)), 
-                new IncomeStatement(500, 200, 70, 30, 300));
+                new BalanceSheet(new Assets(300, 45, 15, 25), new Liabilities(900, 90, 65, 0, 1800)), new IncomeStatement(1800, 250, 40, 30, 275));
         
         FinancialStatementData financialStatementData2014 = new FinancialStatementData(2014, 
-                new BalanceSheet(new Assets(250, 40, 10, 10), new Liabilities(800, 80, 50, 0, 1200)), 
-                new IncomeStatement(450, 150, 70, 30, 200));
+                new BalanceSheet(new Assets(250, 40, 10, 20), new Liabilities(800, 80, 60, 0, 1600)), new IncomeStatement(1600, 200, 35, 30, 250));
         
         FinancialHistory financialHistory = new FinancialHistory(Arrays.asList(financialStatementData2016, financialStatementData2015, financialStatementData2014));
         return new Client("", Industry.AUTOMOTIVE, financialHistory, ExistingLoans.createEmpty());
