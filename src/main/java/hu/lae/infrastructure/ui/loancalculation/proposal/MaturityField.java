09754ecd7de40f0012c1d974cd3acfd16e969df6
Kocso Sandor Gabor <kocso.sandor.gabor@gmail.com>
2017-12-01 22:27:50 +0100

    replace combo with buttons in matrurity selector

--- src/main/java/hu/lae/infrastructure/ui/loancalculation/proposal/MaturityField.java
+++ src/main/java/hu/lae/infrastructure/ui/loancalculation/proposal/MaturityField.java
@@ -1,44 +1,37 @@
 package hu.lae.infrastructure.ui.loancalculation.proposal;
 
 import java.time.LocalDate;
-import java.time.temporal.ChronoUnit;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
+import com.vaadin.ui.Button;
 import com.vaadin.ui.Component;
 import com.vaadin.ui.CustomField;
 import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.themes.ValoTheme;
 
-import hu.lae.infrastructure.ui.component.ComboBox;
 import hu.lae.infrastructure.ui.component.DateField;
 import hu.lae.util.Clock;
 
 @SuppressWarnings("serial")
 class MaturityField extends CustomField<LocalDate> {
 
-    private final ComboBox<Integer> yearsCombo;
-    
     private final DateField dateField;
     
+    private final List<YearButton> yearButtons;
+    
     MaturityField(int maxLoanDuration) {
-        setCaption("Select maturity date or years");
-        yearsCombo = new ComboBox<>(null, generateComboValues(maxLoanDuration));
-        yearsCombo.setItemCaptionGenerator(year -> year + " year" + (year > 1 ? "s" : ""));
-        yearsCombo.setValue(maxLoanDuration);
-        yearsCombo.addStyleName(ValoTheme.COMBOBOX_ALIGN_CENTER);
-        yearsCombo.setWidth("105px");
-        yearsCombo.setEmptySelectionAllowed(false);
+        setCaption("Select maturity date");
         
         dateField = new DateField(null, "loan.request.maturity.date");
         dateField.setRangeStart(Clock.date().plusYears(1));
         dateField.setRangeEnd(Clock.date().plusYears(maxLoanDuration));
         dateField.setValue(Clock.date().plusYears(maxLoanDuration));
-        dateField.setWidth("130px");
+        dateField.setWidth("140px");
         
-        yearsCombo.addValueChangeListener(e -> dateField.setValue(Clock.date().plusYears(e.getValue())));
-        dateField.addValueChangeListener(e -> yearsCombo.setValue((int)ChronoUnit.YEARS.between(Clock.date(), e.getValue())));
+        yearButtons = IntStream.range(1, 6).mapToObj(year -> new YearButton(year, dateField)).collect(Collectors.toList());
+        yearButtons.stream().forEach(button -> button.setEnabled(button.years <= maxLoanDuration));
     }
     
     @Override
@@ -48,7 +41,8 @@ class MaturityField extends CustomField<LocalDate> {
 
     @Override
     protected Component initContent() {
-        return new HorizontalLayout(dateField, yearsCombo);
+    	HorizontalLayout yearButtonsBar = new HorizontalLayout(yearButtons.toArray(new YearButton[0]));
+        return new HorizontalLayout(dateField, yearButtonsBar);
     }
 
     @Override
@@ -56,8 +50,21 @@ class MaturityField extends CustomField<LocalDate> {
         dateField.setValue(value);
     }
     
-    private static List<Integer> generateComboValues(int maxValue) {
-        return IntStream.range(1, maxValue + 1).mapToObj(i -> i).collect(Collectors.toList());
+    static class YearButton extends Button {
+    	
+    	final int years;
+    	
+		public YearButton(int years, DateField dateField) {
+			this.years = years;
+			setCaption(years + " year" + (years > 1 ? "s" : ""));
+			addStyleName(ValoTheme.BUTTON_TINY);
+			
+			setWidth("60px");
+			setHeight("20px");
+			
+			addClickListener(click -> dateField.setValue(Clock.date().plusYears(years)));
+		}
+    	
     }
 
 }
